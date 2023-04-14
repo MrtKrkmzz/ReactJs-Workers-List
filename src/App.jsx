eba921504333776858734c8ad4f0674751d2aba4
Mrtkrkmzz <githubmertkorkmaz@gmail.com>
2023-04-15 01:10:37 +0300

    Güncelleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddWorkers from "./components/workers/AddWorkers";
 import WorkerList from "./components/workers/WorkerList";
 
 function App() {
-  const [workers, setWorkers] = useState([]);
+  const [workers, setWorkers] = useState(localStorage.getItem("workers")
+  ? JSON.parse(localStorage.getItem("workers"))
+  : []
+  );
   const [error,setError]=useState(false);
+  useEffect(()=>{
+    localStorage.setItem("workers",JSON.stringify(workers))
+  },[workers])
   return (
     <div className="App">
       <AddWorkers setWorkers={setWorkers} setError={setError} error={error} />
